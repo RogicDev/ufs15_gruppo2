526527681a87fdc2ff8fe6c2190ae326e954ada3
Mirko Lorenzo La Rocca <153237929+MirkoLorenzoLaRocca@users.noreply.github.com>
2024-12-04 16:34:44 +0100

    Update pokedex.py

--- pokedex.py
+++ pokedex.py
@@ -3,9 +3,9 @@ import pandas as pd
 def pokedex(prediction):
     df = pd.read_csv(rf'data/pokedex.csv')
     row = df[df['Nome'].str.strip().str.lower() == prediction.strip().lower()]
-    description = row['Descrizione'].iloc[0]
-    return description
+    description, tipo = row['Descrizione'].iloc[0], row['Tipo'].iloc[0]
+    return description, tipo
 
 if __name__ == '__main__':
-    description = pokedex('Venusaur')
-    print(description)
+    description, tipo = pokedex('Venusaur')
+    print(f'Descrizione: {description}\nTipo: {tipo}')
